22ce372bc196711d22f6f010dbcb12545bb47138
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2021-10-10 07:42:59 +0200

    version ss21

---
+++ include/visnav/bow_db.h
@@ -0,0 +1,105 @@
+/**
+BSD 3-Clause License
+
+Copyright (c) 2018, Vladyslav Usenko and Nikolaus Demmel.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+#include <fstream>
+
+#include <tbb/concurrent_unordered_map.h>
+#include <tbb/concurrent_vector.h>
+
+#include <visnav/common_types.h>
+#include <visnav/serialization.h>
+
+namespace visnav {
+
+class BowDatabase {
+ public:
+  BowDatabase() {}
+
+  inline void insert(const FrameCamId& fcid, const BowVector& bow_vector) {
+    // TODO SHEET 3: add a bow_vector that corresponds to frame fcid to the
+    // inverted index. You can assume the image hasn't been added before.
+    UNUSED(fcid);
+    UNUSED(bow_vector);
+  }
+
+  inline void query(const BowVector& bow_vector, size_t num_results,
+                    BowQueryResult& results) const {
+    // TODO SHEET 3: find num_results closest matches to the bow_vector in the
+    // inverted index. Hint: for good query performance use std::unordered_map
+    // to accumulate scores and std::partial_sort for getting the closest
+    // results. You should use L1 difference as the distance measure. You can
+    // assume that BoW descripors are L1 normalized.
+    UNUSED(bow_vector);
+    UNUSED(num_results);
+    UNUSED(results);
+  }
+
+  void clear() { inverted_index.clear(); }
+
+  void save(const std::string& out_path) {
+    BowDBInverseIndex state;
+    for (const auto& kv : inverted_index) {
+      for (const auto& a : kv.second) {
+        state[kv.first].emplace_back(a);
+      }
+    }
+    std::ofstream os;
+    os.open(out_path, std::ios::binary);
+    cereal::JSONOutputArchive archive(os);
+    archive(state);
+  }
+
+  void load(const std::string& in_path) {
+    BowDBInverseIndex inverseIndexLoaded;
+    {
+      std::ifstream os(in_path, std::ios::binary);
+      cereal::JSONInputArchive archive(os);
+      archive(inverseIndexLoaded);
+    }
+    for (const auto& kv : inverseIndexLoaded) {
+      for (const auto& a : kv.second) {
+        inverted_index[kv.first].emplace_back(a);
+      }
+    }
+  }
+
+  const BowDBInverseIndexConcurrent& getInvertedIndex() {
+    return inverted_index;
+  }
+
+ protected:
+  BowDBInverseIndexConcurrent inverted_index;
+};
+
+}  // namespace visnav
---
+++ include/visnav/vo_utils.h
@@ -0,0 +1,175 @@
+/**
+BSD 3-Clause License
+
+Copyright (c) 2018, Vladyslav Usenko and Nikolaus Demmel.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+#include <set>
+
+#include <visnav/common_types.h>
+
+#include <visnav/calibration.h>
+
+#include <opengv/absolute_pose/CentralAbsoluteAdapter.hpp>
+#include <opengv/absolute_pose/methods.hpp>
+#include <opengv/relative_pose/CentralRelativeAdapter.hpp>
+#include <opengv/sac/Ransac.hpp>
+#include <opengv/sac_problems/absolute_pose/AbsolutePoseSacProblem.hpp>
+#include <opengv/triangulation/methods.hpp>
+
+namespace visnav {
+
+void project_landmarks(
+    const Sophus::SE3d& current_pose,
+    const std::shared_ptr<AbstractCamera<double>>& cam,
+    const Landmarks& landmarks, const double cam_z_threshold,
+    std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>>&
+        projected_points,
+    std::vector<TrackId>& projected_track_ids) {
+  projected_points.clear();
+  projected_track_ids.clear();
+
+  // TODO SHEET 5: project landmarks to the image plane using the current
+  // locations of the cameras. Put 2d coordinates of the projected points into
+  // projected_points and the corresponding id of the landmark into
+  // projected_track_ids.
+  UNUSED(current_pose);
+  UNUSED(cam);
+  UNUSED(landmarks);
+  UNUSED(cam_z_threshold);
+}
+
+void find_matches_landmarks(
+    const KeypointsData& kdl, const Landmarks& landmarks,
+    const Corners& feature_corners,
+    const std::vector<Eigen::Vector2d,
+                      Eigen::aligned_allocator<Eigen::Vector2d>>&
+        projected_points,
+    const std::vector<TrackId>& projected_track_ids,
+    const double match_max_dist_2d, const int feature_match_threshold,
+    const double feature_match_dist_2_best, LandmarkMatchData& md) {
+  md.matches.clear();
+
+  // TODO SHEET 5: Find the matches between projected landmarks and detected
+  // keypoints in the current frame. For every detected keypoint search for
+  // matches inside a circle with radius match_max_dist_2d around the point
+  // location. For every landmark the distance is the minimal distance between
+  // the descriptor of the current point and descriptors of all observations of
+  // the landmarks. The feature_match_threshold and feature_match_dist_2_best
+  // should be used to filter outliers the same way as in exercise 3. You should
+  // fill md.matches with <featureId,trackId> pairs for the successful matches
+  // that pass all tests.
+  UNUSED(kdl);
+  UNUSED(landmarks);
+  UNUSED(feature_corners);
+  UNUSED(projected_points);
+  UNUSED(projected_track_ids);
+  UNUSED(match_max_dist_2d);
+  UNUSED(feature_match_threshold);
+  UNUSED(feature_match_dist_2_best);
+}
+
+void localize_camera(const Sophus::SE3d& current_pose,
+                     const std::shared_ptr<AbstractCamera<double>>& cam,
+                     const KeypointsData& kdl, const Landmarks& landmarks,
+                     const double reprojection_error_pnp_inlier_threshold_pixel,
+                     LandmarkMatchData& md) {
+  md.inliers.clear();
+
+  // default to previous pose if not enough inliers
+  md.T_w_c = current_pose;
+
+  if (md.matches.size() < 4) {
+    return;
+  }
+
+  // TODO SHEET 5: Find the pose (md.T_w_c) and the inliers (md.inliers) using
+  // the landmark to keypoints matches and PnP. This should be similar to the
+  // localize_camera in exercise 4 but in this exercise we don't explicitly have
+  // tracks.
+  UNUSED(cam);
+  UNUSED(kdl);
+  UNUSED(landmarks);
+  UNUSED(reprojection_error_pnp_inlier_threshold_pixel);
+}
+
+void add_new_landmarks(const FrameCamId fcidl, const FrameCamId fcidr,
+                       const KeypointsData& kdl, const KeypointsData& kdr,
+                       const Calibration& calib_cam, const MatchData& md_stereo,
+                       const LandmarkMatchData& md, Landmarks& landmarks,
+                       TrackId& next_landmark_id) {
+  // input should be stereo pair
+  assert(fcidl.cam_id == 0);
+  assert(fcidr.cam_id == 1);
+
+  const Sophus::SE3d T_0_1 = calib_cam.T_i_c[0].inverse() * calib_cam.T_i_c[1];
+  const Eigen::Vector3d t_0_1 = T_0_1.translation();
+  const Eigen::Matrix3d R_0_1 = T_0_1.rotationMatrix();
+
+  // TODO SHEET 5: Add new landmarks and observations. Here md_stereo contains
+  // stereo matches for the current frame and md contains feature to landmark
+  // matches for the left camera (camera 0). For all inlier feature to landmark
+  // matches add the observations to the existing landmarks. If the left
+  // camera's feature appears also in md_stereo.inliers, then add both
+  // observations. For all inlier stereo observations that were not added to the
+  // existing landmarks, triangulate and add new landmarks. Here
+  // next_landmark_id is a running index of the landmarks, so after adding a new
+  // landmark you should always increase next_landmark_id by 1.
+  UNUSED(fcidl);
+  UNUSED(fcidr);
+  UNUSED(kdl);
+  UNUSED(kdr);
+  UNUSED(calib_cam);
+  UNUSED(md_stereo);
+  UNUSED(md);
+  UNUSED(landmarks);
+  UNUSED(next_landmark_id);
+  UNUSED(t_0_1);
+  UNUSED(R_0_1);
+}
+
+void remove_old_keyframes(const FrameCamId fcidl, const int max_num_kfs,
+                          Cameras& cameras, Landmarks& landmarks,
+                          Landmarks& old_landmarks,
+                          std::set<FrameId>& kf_frames) {
+  kf_frames.emplace(fcidl.frame_id);
+
+  // TODO SHEET 5: Remove old cameras and observations if the number of keyframe
+  // pairs (left and right image is a pair) is larger than max_num_kfs. The ids
+  // of all the keyframes that are currently in the optimization should be
+  // stored in kf_frames. Removed keyframes should be removed from cameras and
+  // landmarks with no left observations should be moved to old_landmarks.
+  UNUSED(max_num_kfs);
+  UNUSED(cameras);
+  UNUSED(landmarks);
+  UNUSED(old_landmarks);
+}
+}  // namespace visnav
